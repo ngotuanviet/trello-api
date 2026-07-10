6bffc51ac83761118407c659912b8592df641724
ngotuanviet <ngotuanviet9@gmail.com>
2026-07-10 15:23:55 +0700

    feat: add production domain to whitelist in constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
 export const WHITELIST_DOMAINS = [
+  'https://trello-api-ieln.onrender.com'
   // 'http://localhost:5173'
   // Không cần localhost nữa vi o file config/cors đã luôn luôn cho phep môi trường
   //dev(env.BUILD_MODE === 'dev')
